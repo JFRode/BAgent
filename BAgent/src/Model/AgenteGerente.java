8ea2361f2bedd25f352231178ebc36149a87f925
5122929 <5122929@U702432.univali.br>
2016-11-04 22:05:45 -0200

    Escrito primeira reação de gerência de atendimento para AgenteGerente

---
+++ BAgent/src/Model/AgenteGerente.java
@@ -0,0 +1,51 @@
+package Model;
+
+import View.JanelaSimulacao;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+/**
+ *
+ * @author Ailton Cardoso Junior
+ *         Antonio Roque Falcão Junior
+ *         Joao Felipe Gonçalves
+ */
+public class AgenteGerente extends Agent{
+
+    private int qtdClientes;
+
+    public AgenteGerente() {
+        this.qtdClientes = 0;
+        this.getAID().setLocalName("Gerente");
+    }
+    
+    @Override
+    protected void setup(){
+        addBehaviour(new OneShotBehaviour(this) {
+
+            @Override
+            public void action() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        addBehaviour(new CyclicBehaviour(this) {
+            
+            @Override
+            public void action() {
+                ACLMessage msg = myAgent.receive();
+                if (msg != null) {
+                    String content = msg.getContent();
+                    if (content.equalsIgnoreCase("Quero ser atendido!")) {
+                        if (JanelaSimulacao.listaClientes.size() >= 6) {
+                            // Envia mensagem para o atendente ir atender
+                        }
+                        
+                    }
+                }
+            }
+        });
+    }
+}
